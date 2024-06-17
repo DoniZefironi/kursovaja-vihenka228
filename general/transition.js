5c95005d5585e034f06580cfdef524d17c9a650f
DoniZefironi <136065842+DoniZefironi@users.noreply.github.com>
2024-06-18 00:38:20 +0300

    feat: add files

---
+++ general/transition.js
@@ -0,0 +1,18 @@
+document.getElementById("sign").addEventListener("click", function() {
+    window.location.href = "../registration/aut.html";
+  });
+
+  document.getElementById("reg").addEventListener("click", function() {
+    window.location.href = "../registration/registr.html";
+  });
+
+  document.getElementById("seepur2").addEventListener("click", function() {
+    window.location.href = "../ourstory/story.html";
+  });
+
+  document.getElementById("seepur1").addEventListener("click", function() {
+    window.location.href = "../function/function.html";
+  });
+  document.getElementById("seepur").addEventListener("click", function() {
+    window.location.href = "../bill/bill.html";
+  });
